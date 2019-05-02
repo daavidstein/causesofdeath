0d6c5f4f5f947e2fcede422673e5872b5c94dd5d
Daavid <daavid.stein18@ncf.edu>
2019-05-02 16:20:35 -0400

    commenting out prof data's code

--- ui.R
+++ ui.R
@@ -15,27 +15,27 @@ dashboardPage(
   ),
   
   dashboardBody(
-    tabItems(
-      tabItem(tabName = "Stat",
-        h2("Expiration of Licenses"),
-        fluidRow(
-          column(3, infoBoxOutput("exp2019", width=NULL) %>% withSpinner(color="green")),
-          column(3, infoBoxOutput("exp2020", width=NULL) %>% withSpinner(color="lightblue"))
-        ),
-        fluidRow(
-          column(3, infoBoxOutput("exp2021", width=NULL) %>% withSpinner(color="lightblue")),
-          column(3, infoBoxOutput("exp2022", width=NULL) %>% withSpinner(color="lightblue"))
-        )
-      ),
-      tabItem(tabName = "Rank",
-        h2("Most Common Expiration Dates:"),
-        fluidRow(
-          box(width=5, status="info", title="Top 5 Expiration Dates", solidHeader = TRUE,
-            tableOutput("top5") %>% withSpinner(color="#0dc5c1")
-          )
-        )
-      )
+    # tabItems(
+    #   tabItem(tabName = "Stat",
+    #     h2("Expiration of Licenses"),
+    #     fluidRow(
+    #       column(3, infoBoxOutput("exp2019", width=NULL) %>% withSpinner(color="green")),
+    #       column(3, infoBoxOutput("exp2020", width=NULL) %>% withSpinner(color="lightblue"))
+    #     ),
+    #     fluidRow(
+    #       column(3, infoBoxOutput("exp2021", width=NULL) %>% withSpinner(color="lightblue")),
+    #       column(3, infoBoxOutput("exp2022", width=NULL) %>% withSpinner(color="lightblue"))
+    #     )
+    #   ),
+    #   tabItem(tabName = "Rank",
+    #     h2("Most Common Expiration Dates:"),
+    #     fluidRow(
+    #       box(width=5, status="info", title="Top 5 Expiration Dates", solidHeader = TRUE,
+    #         tableOutput("top5") %>% withSpinner(color="#0dc5c1")
+    #       )
+    #     )
+    #   )
     )
   )
   
-)
+#)
