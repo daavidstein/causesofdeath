b5835f6aa85a2f91146e7097a7d83e6ed34cd3af
Daavid <daavid.stein18@ncf.edu>
2019-05-02 16:09:19 -0400

    kjni

---
+++ config.R
@@ -0,0 +1,7 @@
+df <- read.socrata(
+  "https://data.cityofnewyork.us/resource/uvxr-2jwn.json",
+  app_token = "EBAtfYRrKbo7tiR4nlngkKp1M",
+  email     = "james.trimarco@gmail.com",
+  password  = "chromo*8Remount"
+)  
+
